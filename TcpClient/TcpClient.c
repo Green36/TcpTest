9166b94c07deff7b648ee961227a54274fbe2849
imo <p@j>
2016-11-27 16:41:13 +0900

    update

--- TcpClient/TcpClient.c
+++ TcpClient/TcpClient.c
@@ -21,6 +21,7 @@
         goto ErrorReturn; \
     }
 
+#define DEBU_PRINT
 
 void sock_print(char* str, int family, int socktype)
 {
@@ -29,7 +30,27 @@ void sock_print(char* str, int family, int socktype)
 
 void sockaddr_print(char* str, struct sockaddr* addr, socklen_t len)
 {
+#ifdef DEBU_PRINT
+    for(int i=0; i<len; i++)
+    {
+        printf("%s sa_family = %d\n", str, addr[i].sa_family);
+        printf("%s sa_data = %s\n", str, addr[i].sa_data);
+    }
+#endif
+}
 
+void addrinfo_print(char* str, struct addrinfo* info)
+{
+#ifdef DEBU_PRINT
+    printf("%s ai_flags = %d\n", str, info->ai_flags );
+    printf("%s ai_family = %d\n", str, info->ai_family );
+    printf("%s ai_socktype = %d\n", str, info->ai_socktype );
+    printf("%s ai_protocol = %d\n", str, info->ai_protocol );
+    printf("%s ai_addrlen = %d\n", str, info->ai_addrlen );
+    sockaddr_print("        ", info->ai_addr, info->ai_addrlen);
+    printf("%s ai_canonname = %s\n", str, info->ai_canonname );
+    printf("%s ai_next = %p\n", str, info->ai_next );
+#endif
 }
 
 // サーバに接続する。
@@ -44,17 +65,22 @@ int connect_to_server(
     struct addrinfo* ai;
 
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;     // IPv4/IPv6両対応
+//    hints.ai_family = AF_UNSPEC;     // IPv4/IPv6両対応
+    hints.ai_family = AF_INET6;     // IPv4/IPv6両対応
     hints.ai_socktype = SOCK_STREAM;
     // serviceはポート番号でなければならない。AI_NUMERICSERV を指定しなければ、
     // 'pop'などでもよい。
     hints.ai_flags = AI_NUMERICSERV;
 
+    printf("hostname = %s, service = %s\n", hostname, service);
+    addrinfo_print("    hints", &hints);
     err = getaddrinfo(hostname, service, &hints, &res);
     if (err != 0) {
-        printf("getaddrinfo(): %s\n", gai_strerror(err));
+        printf("getaddrinfo(): %d(%s)\n", err, gai_strerror(err));
         ERROR_RETURN;
     }
+    addrinfo_print("    hints", &hints);
+    addrinfo_print("    res", res);
 
     for (ai = res; ai; ai = ai->ai_next) {
         sockaddr_print("connect...", ai->ai_addr, ai->ai_addrlen);
@@ -90,7 +116,10 @@ int main()
 
     for(;;)
     {
-        sockfd = connect_to_server(hostname, PORT);
+//        sockfd = connect_to_server(hostname, PORT);
+//        sockfd = connect_to_server("192.168.0.18", PORT);
+        sockfd = connect_to_server("fe80::7462:7e85:f900:6e78/64", PORT);
+
         if (sockfd < 0) {
             perror("client");
             return 1;
